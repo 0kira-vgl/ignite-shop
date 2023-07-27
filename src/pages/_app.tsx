98b75c84f0b6db449b25d8494ad6805cec8ba156
Matheus Henrique <matheusgtiburcio@gmail.com>
2023-07-27 20:33:49 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from "next/app";
+import { GlobalStyles } from "../styles/global";
+import { Container, Header } from "../styles/pages/app";
+import LogoImg from "../assets/igniteLogo.svg";
+import Image from "next/image";
+
+GlobalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={LogoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
+}
